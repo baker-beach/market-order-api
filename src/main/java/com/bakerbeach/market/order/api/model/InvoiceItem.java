ae97de91e7d3505e8bbfe640f3f5adb7208834c9
Mark Hoja <mark.hoja@web.de>
2017-06-21 14:54:41 +0200

    invoice item update & new version

--- src/main/java/com/bakerbeach/market/order/api/model/InvoiceItem.java
+++ src/main/java/com/bakerbeach/market/order/api/model/InvoiceItem.java
@@ -1,5 +1,23 @@
 package com.bakerbeach.market.order.api.model;
 
+import java.math.BigDecimal;
+
+import com.bakerbeach.market.core.api.model.TaxCode;
+
 public interface InvoiceItem {
+	
+	String getGtin();
+
+	String getName();
+
+	BigDecimal getQuantity();
+	
+	TaxCode getTaxCode();
+	
+	BigDecimal getTaxPercent();
+
+	BigDecimal getTotal();
+	
+	BigDecimal getUnitPrice();
 
 }
